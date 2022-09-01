dd705f37ec582ffc9ac0dcf01490c4b1075a84a8
D-Bros <112539248+D-Bros@users.noreply.github.com>
2022-09-01 12:57:59 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,26 @@
-print("Hello, World")
+words =  ['father', 'enterprise', 'science', 'programming', 'resistance', 'fiction', 'condition', 'reverse', 'computer', 'python']
+word = random.choice(words)
+
+guesses = ''
+
+turns = 10
+while turns > 0:
+    print(f"you have left {turns} turns ")
+    
+    guesses_all = True
+    for c in word:
+        if c in guesses:
+            print(c,end=' ')
+        else:
+            print('_',end=' ')
+            guesses_all = False
+    print()
+    
+    if not guesses_all:
+        guess = input("Guess char  ")
+        guesses = guesses + guess
+        if guess not in word:
+            turns = turns - 1
+    else:
+        print("you win")
+        turns =0
